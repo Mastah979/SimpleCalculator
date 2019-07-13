c9f9508b1e89f1f91c7e848f33a7b35853bbcb1e
damian szalinski <dszalinski979@gmail.com>
2019-07-13 12:38:06 +0200

    update

--- SimpleCalculator/InputConverter.cs
+++ SimpleCalculator/InputConverter.cs
@@ -10,10 +10,14 @@ namespace SimpleCalculator
 
         public double ConvertInputToNumeric(string textInput)
         {
+            //metoda dostepna od C# 7 (mozna w instrukcji warunkowej "?" zwrocic exception w razie niepowodzenia)
+            return double.TryParse(textInput, out double convertedNumber) ? convertedNumber : throw new ArgumentException("Expected a numeric value.");
+
+
             
-            if (!double.TryParse(textInput, out double convertedNumber))
-                throw new ArgumentException("Expected a numeric value.");
-            return convertedNumber;
+            //if (!double.TryParse(textInput, out double convertedNumber))
+            //    throw new ArgumentException("Expected a numeric value.");
+            //return convertedNumber;
             
 
         }
